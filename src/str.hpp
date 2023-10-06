6604a3397cd567d6d5a4d2e8407abaff11a66207
Zijian Xu <znx418@gmail.com>
2023-10-06 22:08:51 +0200

    feat: str iterator struct

--- src/str.hpp
+++ src/str.hpp
@@ -1,24 +1,142 @@
 #pragma once
 
+#include <bits/types/struct_itimerspec.h>
 #include <cstddef>
+#include <iterator>
 #include <limits>
 #include <utility>
 
 class str
 {
 public:
+
+    class iterator
+    {
+    public:
+        using iterator_category = std::contiguous_iterator_tag;
+        using difference_type = std::ptrdiff_t;
+        using value_type = char;
+        using reference = value_type&;
+        using const_reference = const value_type&;
+        using pointer = value_type*;
+        using const_pointer = const value_type*;
+
+        iterator(pointer ptr) : m_ptr{ptr}
+        {}
+
+        reference operator*()
+        { return *m_ptr; }
+
+        const_reference operator*() const
+        { return *m_ptr; }
+
+        pointer operator->()
+        { return m_ptr; }
+
+        const_pointer operator->() const
+        { return m_ptr; }
+
+        iterator& operator++()
+        {
+            ++m_ptr;
+            return *this;
+        }
+
+        iterator operator++(int)
+        {
+            auto tmp = *this;
+            ++(*this);
+            return tmp;
+        }
+
+        iterator& operator--()
+        {
+            --m_ptr;
+            return *this;
+        }
+
+        iterator operator--(int)
+        {
+            auto tmp = *this;
+            --(*this);
+            return tmp;
+        }
+
+        iterator operator+(difference_type diff)
+        {
+            auto tmp = *this;
+            tmp += diff;
+            return tmp;
+        }
+
+        iterator& operator+=(difference_type diff)
+        {
+            m_ptr += diff;
+            return *this;
+        }
+
+        iterator operator-(difference_type diff)
+        {
+            auto tmp = *this;
+            tmp -= diff;
+            return tmp;
+        }
+
+        iterator& operator-=(difference_type diff)
+        {
+            m_ptr -= diff;
+            return *this;
+        }
+
+        friend bool operator==(const iterator& lhs, const iterator& rhs)
+        {
+            return lhs.m_ptr == rhs.m_ptr;
+        }
+
+        friend bool operator<(const iterator& lhs, const iterator& rhs)
+        {
+            return lhs.m_ptr < rhs.m_ptr;
+        }
+
+        friend bool operator!=(const iterator& lhs, const iterator& rhs)
+        {
+            return !(lhs == rhs);
+        }
+
+
+        friend bool operator<=(const iterator& lhs, const iterator& rhs)
+        {
+            return lhs == rhs || lhs < rhs;
+        }
+
+        friend bool operator>(const iterator& lhs, const iterator& rhs)
+        {
+            return !(lhs <= rhs);
+        }
+
+        friend bool operator>=(const iterator& lhs, const iterator& rhs)
+        {
+            return !(lhs < rhs);
+        }
+
+    private:
+        pointer m_ptr;
+    };
+
+    // TODO reverse iterator
+
     str() = default;
 
     str(const char*);
 
     str(char*&&);
 
-    ~str()
-    { delete[] m_str; }
-
     str(const str&);
 
     str(str&&);
+
+    ~str()
+    { delete[] m_str; }
 
     str& operator=(str);
 
@@ -50,6 +168,12 @@ public:
 
     char& back() const
     { return const_cast<str&>(*this).back(); }
+
+    iterator begin()
+    { return iterator(&m_str[0]); }
+
+    iterator end()
+    { return iterator(&m_str[m_len]); }
 
     void push_back(char);
 
